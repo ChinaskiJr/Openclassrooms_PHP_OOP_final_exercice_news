8f61f11de5d4989df175d30a2c1ad930f0e91813
ChinaskiJr <robin.colombier@gmail.com>
2018-07-28 16:15:03 +0200

    set the class to instanciate client's responses

---
+++ lib/OCFram/HTTPResponse.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace OCFram;
+/**
+ * The client's response will be represented as an instance of this class.
+ * 
+ * @version 1.0
+ */
+class HTTPResponse {
+	/**
+	 * @var An instance of Page
+	 */
+	protected $page;
+	/**
+	 * Add a specific header to the response.
+	 * @param string $header
+	 * @return void
+	 */
+	public function addHeader($header) {
+		header($header);
+	}
+	/**
+	 * Redirect the client to a specific location.
+	 * @param $string location
+	 * @return void
+	 */
+	public function redirect($location) {
+		header('Location : ' . $location);
+		exit;
+	}
+	/** 
+	 * Redirect the client to the 404 default page.
+	 * @return void
+	 */
+	public function redirect404() {
+		// WAIT FOR THE PAGE CLASS TO BE CREATED
+	}
+	public function send() {
+		exit($this->page()->getGeneratedPage());
+	}
+	/**
+	 * Set a cookie to send the client.
+	 * @param string $name.
+	 * @param string $value Optionnal. A whitespace by default.
+	 * @param string $path Optionnal. null by default.
+	 * @param string $domain Optionnal. null by default.
+	 * @param bool $secure Optionnal. false by default.
+	 * @param bool $httpOnly. Optionnal. true by default.
+	 * @return void
+	 */
+	public function setCookie ($name, $value = ' ', $path = null, $domain = null, $secure = false, $httpOnly = true) {
+		setcookie($name, $value, $path, $domain, $secure, $httpOnly);
+	}
+	/**
+	 * Setter for $page
+	 * @param Page $page
+	 * @return void
+	 */
+	public function setPage(Page $page) {
+		$this->page = $page;
+	}
+
+}
